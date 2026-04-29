9f39481fb93c004f545029a4bfbfb09014489c06
DeepakReddy08K <your-email-used-in-github>
2026-04-29 12:00:10 +0530

    no need of geoCode api so removed it and add openWeather api

--- backend/index.js
+++ backend/index.js
@@ -13,18 +13,12 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port=3000;
-async function getCityCoordinates(city) {
-    const api_key=process.env.GEOCODE_API_KEY;
-    const url=`http://api.positionstack.com/v1/forward?access_key=${api_key}&query=${city}`;
+async function getWeatherConditions(city) {
+    const api_key=process.env.OPENWEATHER_API_KEY;
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
     const response=await fetch(url);
     const data=await response.json();
-    console.log("Number of citys found ="+data.data.length);
-      if (data.data.length === 0) {
-         return "error";
-     }
-     let latitude=data.data[0].latitude;
-     let longitude=data.data[0].longitude;
-     return {latitude,longitude};
+    console.log(data);
 }
 app.get("/",(req,res)=>{
     console.log("new get request!");
@@ -32,14 +26,10 @@ app.get("/",(req,res)=>{
 });
 app.post("/submit",(req,res)=>{
     console.log("new post request");
-    let city=req.body.location;
+    const city = req.body.location?.trim();
     console.log(city);
-    const coordiants=getCityCoordinates(city);
-    if(coordiants==="error"){
-        console.log("error");
-        res.send("<h1>Sorry</h1>");
-    }
-    else res.send("<h1>Thank you</h1>");
+    getWeatherConditions(city);
+    res.send("<h1>Thank you</h1>");
 });
 app.listen(port,()=>{
     console.log("sever running on port "+port);
